836a080b6a838103180e6f305bfe3cf53794419c
Daniel… <danieloluwafolajimi@gmail.com>
2024-11-18 22:17:40 +0100

    refactor: Add additional login routes and clean up code formatting

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
-const port = 4000;
+const app = express()
+const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+  res.send('Hello, world!')
+})
+
+app.get('/login/porters', (req, res) => {
+  res.send('Login for porters')
+})
+app.get('/login/nurses', (req, res) => {
+  res.send('Login for nurses')
+})
+app.get('/login/chaplaincy', (req, res) => {
+  res.send('Login for chaplaincy')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  console.log(`Server is running on http://localhost:${port}`)
+})
