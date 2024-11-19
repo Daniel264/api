2fd81e003fcc6b8845974785515f56426e1a214f
Daniel… <danieloluwafolajimi@gmail.com>
2024-11-19 22:53:43 +0100

    refactor: update user registration route with validation and error handling

--- index.ts
+++ index.ts
@@ -4,14 +4,13 @@ import mongoose from 'mongoose'
 import users from './routes/user'
 import express from 'express'
 const app = express()
-const port = 4000
 
 mongoose
   .connect('mongodb://localhost/hexa')
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error('Could not connect to MongoDb'))
 
-app.use(express.json());
+app.use(express.json())
 app.use('/api/users', users)
 
 app.get('/', (req, res) => {
@@ -28,6 +27,7 @@ app.get('/login/chaplaincy', (req, res) => {
   res.send('Login for chaplaincy')
 })
 
+const port = process.env.PORT || 4000
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
--- routes/user.ts
+++ routes/user.ts
@@ -4,8 +4,19 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-  const users = await User.find().sort('name')
-  res.send(users)
+  const { error } = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  let user = await User.findOne({ staffId: req.body.staffId })
+  if (user) return res.status(400).send('User already registered.')
+
+  user = new User({
+    name: req.body.name,
+    staffId: req.body.staffId,
+    password: req.body.password,
+  })
+  await user.save()
+  res.send(user)
 })
 
 export default router
